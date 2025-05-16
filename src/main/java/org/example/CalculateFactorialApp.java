e5bd4a86c41856c475046074b08b18198fbf3707
Pavlo <pppp_84@ukr.net>
2025-05-16 07:39:53 +0300

    added reading and writing using files

--- src/main/java/org/example/CalculateFactorialApp.java
+++ src/main/java/org/example/CalculateFactorialApp.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.*;
 
 public class CalculateFactorialApp {
     int maxCalculationsForSecond = 100;
+    String inputFileName = "src/main/resources/input.txt";
+    String outputFileName = "src/main/resources/output.txt";
     int numberOfThreads;
     public CalculateFactorialApp(int numberOfThreads) {
         this.numberOfThreads = numberOfThreads;
@@ -23,16 +27,20 @@ public class CalculateFactorialApp {
         LinkedBlockingQueue<String> inputQueue = new LinkedBlockingQueue<>();
         LinkedBlockingQueue<String> outputQueue = new LinkedBlockingQueue<>();
         CountDownLatch countDownLatch = new CountDownLatch(numberOfThreads);
-        inputQueue.offer("1");
-        inputQueue.offer("2");
-        inputQueue.offer("3");
-        inputQueue.offer("4");
-        inputQueue.offer("5");
-        inputQueue.offer("6");
-        inputQueue.offer("7");
-        inputQueue.offer("8");
-        inputQueue.offer("9");
-        inputQueue.offer("10");
+//        inputQueue.offer("1");
+//        inputQueue.offer("2");
+//        inputQueue.offer("3");
+//        inputQueue.offer("4");
+//        inputQueue.offer("5");
+//        inputQueue.offer("6");
+//        inputQueue.offer("7");
+//        inputQueue.offer("8");
+//        inputQueue.offer("9");
+//        inputQueue.offer("10");
+
+        NumbersProducer producer = new NumbersProducer(inputQueue, inputFileName);
+        Thread readingThread = new Thread(producer);
+        readingThread.start();
 
         System.out.println("inputQueue = " + inputQueue);
 
@@ -72,6 +80,9 @@ public class CalculateFactorialApp {
             }
         }
         System.out.println("result: " + outputQueue);
+        SolutionsConsumer consumer = new SolutionsConsumer(outputQueue, outputFileName);
+        Thread writingThread = new Thread(consumer);
+        writingThread.start();
 
     }
 
@@ -118,5 +129,66 @@ class FactorialThread implements Callable<String> {
         }
         return result;
     }
+}
 
+class NumbersProducer implements Runnable{
+
+    BlockingQueue<String> queueFromFile;
+    String inputFilePath;
+
+    public NumbersProducer(BlockingQueue<String> queueFromFile, String inputFilePath) {
+        this.queueFromFile = queueFromFile;
+        this.inputFilePath = inputFilePath;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+//            InputStreamReader stream = new InputStreamReader(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(inputFilePath)));
+            File inputFile = new File(inputFilePath);
+//            BufferedReader reader =
+//                    new BufferedReader(stream);
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                queueFromFile.offer(line);
+            }
+            reader.close();
+//            stream.close();
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class SolutionsConsumer implements Runnable {
+
+    BlockingQueue<String> queueToFile;
+    String outputFileName;
+
+    public SolutionsConsumer(BlockingQueue<String> queueToFile, String outputFileName) {
+        this.queueToFile = queueToFile;
+        this.outputFileName = outputFileName;
+    }
+
+    @Override
+    public void run() {
+        try {
+            File outputFile = new File(outputFileName);
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            String line;
+            while ((line = queueToFile.poll()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+            writer.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
