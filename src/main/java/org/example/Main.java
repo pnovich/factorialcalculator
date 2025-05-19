e332bf272c277ebde52edfc9be1ec293c0f2da3b
Pavlo <pppp_84@ukr.net>
2025-05-19 06:13:46 +0300

    numbers of threads input validation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,29 @@ import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        int n = readNumberOfThreads();
         int maxCalculationsForSecond = 100;
         CalculateFactorialApp app = new CalculateFactorialApp(n, maxCalculationsForSecond);
         app.proccessFactorial();
     }
+
+    private static Integer readNumberOfThreads() {
+        Integer n = null;
+        while (n == null) {
+            System.out.println("Enter the number of threads you want to run :");
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            n = validateInput(input);
+        }
+        return n;
+    }
+
+    private static Integer validateInput(String input) {
+        if (!input.matches("\\d+") || Integer.parseInt(input) < 1) {
+            System.out.println("input should be integer positive number");
+            return null;
+        }
+        return Integer.parseInt(input);
+    }
+
 }
